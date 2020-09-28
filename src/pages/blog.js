7fa7e508cc1be9536c13f613137fbf8e2716077e
Stephanie Fernandes <steph3001@gmail.com>
2020-09-28 19:36:49 -0300

    Added some blog posts

---
+++ src/pages/blog.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import {graphql, useStaticQuery} from 'gatsby'
+
+import Layout from '../components/layout'
+
+const BlogPage = () => {
+  const data =  useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  
+  `)
+
+  return (
+    <Layout>
+      <h1>Blog</h1>
+      <ol>
+        {data.allMarkdownRemark.edges.map(post => (
+          <li>
+            <h2>{post.node.frontmatter.title}</h2>
+            <p>{post.node.frontmatter.date}</p> 
+          </li>
+        ))}
+      </ol>
+    </Layout>
+  )
+}
+
+export default BlogPage
---
+++ src/posts/react.md
@@ -0,0 +1,12 @@
+---
+title: "React"
+date: "2020-04-19"
+---
+
+In this post you'll learn React.
+
+## Topics Covered
+
+1. Basics
+2. Props
+3. State
